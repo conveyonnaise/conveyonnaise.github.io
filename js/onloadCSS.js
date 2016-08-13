750fe4d40b81033d1d8252b19ac6a523cee6fc27
lilianliu52 <lilianliu52@users.noreply.github.com>
2016-08-13 00:47:36 -0700

    Create onloadCSS.js

---
+++ js/onloadCSS.js
@@ -0,0 +1,1 @@
+function onloadCSS(ss,callback){ss.onload=function(){ss.onload=null;if(callback){callback.call(ss);}};if("isApplicationInstalled"in navigator&&"onloadcssdefined"in ss){ss.onloadcssdefined(callback);}}
